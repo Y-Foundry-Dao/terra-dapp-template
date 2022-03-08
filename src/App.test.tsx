f73719f10b732d95f9ce9d21e30d565b818f8de8
Trenten Grede <trenten.grede@gmail.com>
2022-03-08 12:36:16 -0600

    Added a basic test case for the app component to ensure setup is correct

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('hello world renders', () => {
+  render(<App />);
+  const text = screen.getByText(/^hello world$/i);
+  expect(text).toBeInTheDocument();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { ConnectSample } from 'components/ConnectSample';
-import { CW20TokensSample } from 'components/CW20TokensSample';
-import { NetworkSample } from 'components/NetworkSample';
-import { QuerySample } from 'components/QuerySample';
-import { SignBytesSample } from 'components/SignBytesSample';
-import { SignSample } from 'components/SignSample';
-import { TxSample } from 'components/TxSample';
+// import React from 'react';
+// import { ConnectSample } from 'components/ConnectSample';
+// import { CW20TokensSample } from 'components/CW20TokensSample';
+// import { NetworkSample } from 'components/NetworkSample';
+// import { QuerySample } from 'components/QuerySample';
+// import { SignBytesSample } from 'components/SignBytesSample';
+// import { SignSample } from 'components/SignSample';
+// import { TxSample } from 'components/TxSample';
 
 export default function App() {
   return (
     <main>
-      <ConnectSample />
+      {/* <ConnectSample />
       <QuerySample />
       <TxSample />
       <SignSample />
       <SignBytesSample />
       <CW20TokensSample />
-      <NetworkSample />
+      <NetworkSample /> */}
+      <p>Hello World</p>
     </main>
   );
 }
