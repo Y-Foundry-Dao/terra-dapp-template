19b5b25d79de72f84519eeba87f747f6f0e4b79a
Trenten Grede <trenten.grede@gmail.com>
2022-03-08 18:51:03 -0600

    test commit for husky, removed commented code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,6 @@ import App from './App';
 
 test('hello world renders', () => {
   render(<App />);
-
   const text = screen.getByText(/^hello world$/i);
-
   expect(text).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-// import React from 'react';
-// import { ConnectSample } from 'components/ConnectSample';
-// import { CW20TokensSample } from 'components/CW20TokensSample';
-// import { NetworkSample } from 'components/NetworkSample';
-// import { QuerySample } from 'components/QuerySample';
-// import { SignBytesSample } from 'components/SignBytesSample';
-// import { SignSample } from 'components/SignSample';
-// import { TxSample } from 'components/TxSample';
 import Input from 'components/inputs/Input';
 
-const two = 2;
 export default function App() {
   return (
     <main>
-      {/* <ConnectSample />
-      <QuerySample />
-      <TxSample />
-      <SignSample />
-      <SignBytesSample />
-      <CW20TokensSample />
-      <NetworkSample /> */}
       <p>Hello World</p>
       <Input />
     </main>
