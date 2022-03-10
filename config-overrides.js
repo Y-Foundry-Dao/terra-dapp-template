6d79708e535cb9a7970725bfeac1e00a9387b22c
Trenten Grede <trenten.grede@gmail.com>
2022-03-10 15:29:58 -0600

    added env property and a no-var-requires rule in the eslint file

--- config-overrides.js
+++ config-overrides.js
@@ -4,14 +4,14 @@ module.exports = function override(config, env) {
   config.resolve.fallback = {
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
-    buffer: require.resolve('buffer'),
+    buffer: require.resolve('buffer')
   };
 
   config.plugins.push(
     new ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
-      process: 'process/browser',
-    }),
+      process: 'process/browser'
+    })
   );
 
   return config;
